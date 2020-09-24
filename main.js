e8fae1c6a1759735de35941195054c87a8af362e
Mallory Adams <mallory@dxw.com>
2020-09-24 09:40:41 -0400

    MVP - make it update the rich text based on the contents of the textarea

--- main.js
+++ main.js
@@ -11,24 +11,37 @@ function addButton(element, label, callback) {
   element.appendChild(li)
 }
 
-function removeAllChildren(element) {
-  while (element.firstChild) {
-    element.removeChild(element.firstChild)
-  }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-window.addEventListener('load', function () {
-  console.log('hello')
+window.addEventListener('load', async function () {
+  // wait until the editor is loaded (if it ever gets loaded)
+  while (true) {
+    await sleep(100)
+    const editor = document.querySelector('#editor0')
+    if (editor !== null) {
+      run(editor)
+      break
+    }
+  }
+})
 
-  const editor = document.querySelector('#editor0')
+function run(editor) {
   const toolbar = editor.querySelector('.zendesk-editor--custom-tools')
   const richText = editor.querySelector('.zendesk-editor--rich-text-comment')
 
-  addButton(toolbar, 'Markdown', function () {
-    console.log('world')
-    removeAllChildren(richText)
-    const p=document.createElement('p')
-    p.innerText='hello world'
-    richText.appendChild(p)
+  const textarea = document.createElement('textarea')
+  textarea.style.backgroundColor = '#ddd'
+  editor.insertBefore(textarea, editor.firstChild)
+
+  textarea.addEventListener('input', function () {
+    const input = textarea.value
+    const output = marked(input)
+    richText.innerHTML = output
   })
-})
+
+  addButton(toolbar, 'Markdown?', function () {
+    alert('This is a browser extension. https://github.com/dxw/zendesk-markdown')
+  })
+}
